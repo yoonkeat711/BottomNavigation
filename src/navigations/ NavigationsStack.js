143a8c05423f827e230108ca9a51284b025740ab
yoonkeat <yoonkeat711@gmail.com>
2020-10-27 17:32:04 +0800

    added icon and badge

--- src/navigations/ NavigationsStack.js
+++ src/navigations/ NavigationsStack.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useState } from 'react';
+import { Image, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import Calls from '../screens/Calls';
@@ -7,33 +7,91 @@ import Camera from '../screens/Camera';
 import Chats from '../screens/Chats';
 import Settings from '../screens/Settings';
 import Status from '../screens/Status';
+
+import StatusIcon from './../assets/status.png';
 import CameraIcon from './../assets/camera.png';
+import ChatIcon from './../assets/chat.png';
+import CallIcon from './../assets/call.png';
+import SettingsIcon from './../assets/settings.png';
+
+const THEME_COLOR = '#3333FF';
+
 const Tab = createBottomTabNavigator();
 
-const BottomTab = () => {
+const TabBarIcon = ({ icon, focused, customBadge }) => {
     return (
-        <Tab.Navigator initialRouteName={"Status"} tabBarOptions={{ activeTintColor: 'blue' }}>
+        <>
+            {customBadge && <View style={{ position: 'absolute', top: 8, right: 25, backgroundColor: THEME_COLOR, height: 8, width: 8, borderRadius: 4 }} />}
+            <Image
+                source={icon}
+                resizeMode={'contain'}
+                style={{ height: 30, width: 30, tintColor: focused ? THEME_COLOR : 'grey' }}
+            />
+        </>
+    )
+}
+
+const BottomTab = () => {
+    const [chatBadgeNumber, setChatBadgeNumber] = useState(3);
+    const [settingsBadgeNumber, setSettingsBadgeNumber] = useState(1);
+
+    const onDismissChatBadgeNumber = (e) => {
+        setChatBadgeNumber(null);
+    }
+
+    const onDismissSettingsBadgeNumber = () => {
+        setSettingsBadgeNumber(null);
+    }
+
+    return (
+        <Tab.Navigator initialRouteName={"Status"} tabBarOptions={{ activeTintColor: THEME_COLOR }}>
             <Tab.Screen
                 name={"Status"}
                 component={Status}
-                options={{ tabBarIcon: () => <Image source={CameraIcon} style={{ height: 30, width: 30 }} /> }}
+                options={{
+                    tabBarIcon: ({ focused }) =>
+                        <TabBarIcon focused={focused} icon={StatusIcon} customBadge={true} />,
+                }}
             />
             <Tab.Screen
                 name={"Calls"}
                 component={Calls}
-                options={{ tabBarIcon: () => <Image source={CameraIcon} style={{ height: 30, width: 30 }} />, tabBarBadge: 3 }}
+                options={{
+                    tabBarIcon: ({ focused }) =>
+                        <TabBarIcon focused={focused} icon={CallIcon} />
+                }}
             />
             <Tab.Screen
                 name={"Camera"}
                 component={Camera}
+                options={{
+                    tabBarIcon: ({ focused }) =>
+                        <TabBarIcon focused={focused} icon={CameraIcon} />
+                }}
             />
             <Tab.Screen
                 name={"Chats"}
                 component={Chats}
+                options={{
+                    tabBarIcon: ({ focused }) =>
+                        <TabBarIcon focused={focused} icon={ChatIcon} />,
+                    tabBarBadge: chatBadgeNumber,
+                }}
+                listeners={({ navigation, route }) => ({
+                    tabPress: () => onDismissChatBadgeNumber()
+                })}
             />
             <Tab.Screen
                 name={"Settings"}
                 component={Settings}
+                options={{
+                    tabBarIcon: ({ focused }) =>
+                        <TabBarIcon focused={focused} icon={SettingsIcon} />,
+                    tabBarBadge: settingsBadgeNumber,
+                }}
+                listeners={({ navigation, route }) => ({
+                    tabPress: () => onDismissSettingsBadgeNumber()
+                })}
             />
         </Tab.Navigator>
     )
